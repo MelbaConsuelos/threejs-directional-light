2c836c60188a0056f94de1958476fcfe153aca27
Paola Villarreal García <31547279+paolaale@users.noreply.github.com>
2021-04-25 21:46:27 -0500

    helper

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 import * as THREE from "/build/three.module.js";
 import Stats from "/js/jsm/libs/stats.module.js";
-import {OrbitControls} from "/js/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "/js/jsm/controls/OrbitControls.js";
 import * as dat from "/js/jsm/libs/dat.gui.module.js";
 
 "use strict";
 
-let renderer, scene, camera, mesh, cameraControls, gui, stats, directionalLight;
+let renderer, scene, camera, box, cameraControls, gui, stats, directionalLight;
 window.anim = false;
 
 function init(event) {
     // RENDERER ENGINE
-    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setClearColor(new THREE.Color(0, 0, 0));
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
@@ -28,48 +28,66 @@ function init(event) {
     cameraControls = new OrbitControls(camera, renderer.domElement);
 
     // LIGHT
-    let lightColor = "#f9f9f9";
+    let pointLightColor = "white";
     let lightIntensity = 2;
-    directionalLight = new THREE.DirectionalLight( lightColor, lightIntensity );
+    directionalLight = new THREE.DirectionalLight(pointLightColor, lightIntensity);
     directionalLight.position.set(2, 5, 6);
     directionalLight.target.position.set(0, 3, 0);
-    console.log(directionalLight);
+    let directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight);
 
-            
     // MODEL
     // BOX
-    const cubeSize = 3;
-    const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-    const cubeMat = new THREE.MeshPhongMaterial({color: '#8AC'});
-    mesh = new THREE.Mesh(cubeGeo, cubeMat);
-    mesh.position.set(0, 0.5, 0);
-    
+    let geometry = new THREE.BoxGeometry(1);
+    let material = new THREE.MeshPhongMaterial({ color: '#8AC' });
+    box = new THREE.Mesh(geometry, material);
+    box.position.set(0, 0.5, 0);
 
     // FLOOR
     let floor = new Floor();
 
     // SCENE HIERARCHY
-    scene.add(mesh);
+    scene.add(box);
     scene.add(floor);
     scene.add(directionalLight);
     scene.add(directionalLight.target);
-
+    scene.add(directionalLightHelper);
 
     // GUI
     gui = new dat.GUI();
+
     // SHOW/HIDE FLOOR
-    gui.add(floor, "visible").name("Floor").setValue(false).listen().onChange(function(value) {
+    gui.add(floor, "visible").name("Floor").setValue(false).listen().onChange(function (value) {
 
     });
-    gui.add(window, "anim").name("Animation").listen().onChange(function(value) {
+    gui.add(window, "anim").name("Animation").listen().onChange(function (value) {
 
     });
 
-    gui.addColor(new ColorGUIHelper(directionalLight, 'color'), 'value').name('color');
-    gui.add(directionalLight, 'intensity', 0, 2, 0.01);
-    gui.add(directionalLight.target.position, 'x', -10, 10);
-    gui.add(directionalLight.target.position, 'z', -10, 10);
-    gui.add(directionalLight.target.position, 'y', 0, 10);
+    gui.add(directionalLight, "intensity").min(0).max(2).step(0.01).setValue(2).name("Intensity").listen().onChange(function (value) {
+
+    });
+
+    gui.add(directionalLight.target.position, "x").min(-10).max(10).step(1).setValue(0).name("Target X").listen().onChange(function (value) {
+        directionalLightHelper.update();
+    });
+    gui.add(directionalLight.target.position, "y").min(-10).max(10).step(1).setValue(0).name("Target Y").listen().onChange(function (value) {
+        directionalLightHelper.update();
+    });
+    gui.add(directionalLight.target.position, "z").min(-10).max(10).step(1).setValue(0).name("Target Z").listen().onChange(function (value) {
+        directionalLightHelper.update();
+    });
+
+    let params = {
+        boxColor: new THREE.Color(1, 1, 1),
+        directionalLightColor: new THREE.Color(1, 1, 1)
+    }
+    gui.addColor(params, "boxColor").name("Box Color").listen().onChange(function (color) {
+        box.material.color = new THREE.Color(color.r / 255, color.g / 255, color.b / 255);
+    });
+    gui.addColor(params, "directionalLightColor").name("Light Color").listen().onChange(function (color) {
+        directionalLight.color = new THREE.Color(color.r / 255, color.g / 255, color.b / 255);
+    });
+
     gui.open();
 
     // SETUP STATS
@@ -91,9 +109,9 @@ function renderLoop() {
 }
 
 function updateScene() {
-    if(anim) {
-        mesh.rotation.x = mesh.rotation.x + 0.01;
-        mesh.rotation.y = mesh.rotation.y + 0.01;
+    if (anim) {
+        box.rotation.x = box.rotation.x + 0.01;
+        box.rotation.y = box.rotation.y + 0.01;
     }
 }
 
@@ -122,17 +140,5 @@ class Floor extends THREE.Mesh {
     }
 }
 
-class ColorGUIHelper {
-    constructor(object, prop) {
-      this.object = object;
-      this.prop = prop;
-    }
-    get value() {
-      return `#${this.object[this.prop].getHexString()}`;
-    }
-    set value(hexString) {
-      this.object[this.prop].set(hexString);
-    }
-  }
 
 
